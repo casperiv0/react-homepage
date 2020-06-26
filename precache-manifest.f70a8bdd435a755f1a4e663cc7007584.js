4005a0183b3c2e4d6fe825db00a10806770f4320
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-06-26 20:53:57 +0200

    Updates

---
+++ precache-manifest.f70a8bdd435a755f1a4e663cc7007584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0bdcf6fa33cf36d0fa9d3c88dc00fff",
+    "url": "/react-homepage/index.html"
+  },
+  {
+    "revision": "b4590f7d3b3fe0af2b07",
+    "url": "/react-homepage/static/css/main.6beaedcb.chunk.css"
+  },
+  {
+    "revision": "670a6f019debf0b390a4",
+    "url": "/react-homepage/static/js/2.fc7a2c6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-homepage/static/js/2.fc7a2c6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4590f7d3b3fe0af2b07",
+    "url": "/react-homepage/static/js/main.65664e0c.chunk.js"
+  },
+  {
+    "revision": "c5ec1129d35c38fd4108",
+    "url": "/react-homepage/static/js/runtime-main.1a244970.js"
+  }
+]);
